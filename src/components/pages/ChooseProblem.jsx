867473d77bb600a36596221182bb077f21319324
Nature711 <e0665488@u.nus.edu>
2021-12-11 16:33:15 +0800

    Improve ChooseProblem Page

--- src/components/pages/ChooseProblem.jsx
+++ src/components/pages/ChooseProblem.jsx
@@ -1,29 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import background from "../../backgrounds/choose-problem-galaxy.mp4";
 import styled from "styled-components/macro";
 import { VideoBackground } from "../../globalStyles";
-import { TextContainer } from "../../globalStyles";
-
-const InnerWrapper = styled.div`
-  position: absolute;
-  top: 30%;
-  width: 100%;
-  height: 300px;
-  display: flex;
-  justify-content: center;
-`;
+import {
+  TextContainer,
+  LinkContainer,
+  LightButton,
+  InnerWrapper,
+} from "../../globalStyles";
 
 function ChooseProblem() {
+  const [displayEmotion, setDisplayEmotion] = useState(true);
+  const [emotion, setEmotion] = useState("");
+  const [displayEvent, setDisplayEvent] = useState(false);
+  const [event, setEvent] = useState("");
+
+  const handleSelectEmotion = (emotion) => {
+    setEmotion(emotion);
+    setDisplayEmotion(false);
+    setDisplayEvent(true);
+  };
+
+  const handleSelectEvent = (event) => {
+    setEvent(event);
+    setDisplayEvent(false);
+  };
+
   return (
     <>
       <VideoBackground autoPlay muted loop playsInline>
         <source src={background} type="video/mp4" />
       </VideoBackground>
       <InnerWrapper>
-        <TextContainer>
-          Tell me how you feel... <br />
-          And why you feel this way?
-        </TextContainer>
+        {displayEmotion ? (
+          <>
+            <TextContainer>Tell me how you feel...</TextContainer>
+            <LinkContainer>
+              <LightButton onClick={() => handleSelectEmotion("Sad")}>
+                Sad
+              </LightButton>
+              <LightButton onClick={() => handleSelectEmotion("Anxious")}>
+                Anxious
+              </LightButton>
+              <LightButton onClick={() => handleSelectEmotion("Angry")}>
+                Angry
+              </LightButton>
+            </LinkContainer>
+          </>
+        ) : (
+          <>
+            {displayEvent ? (
+              <>
+                <TextContainer>What makes you feel this way...?</TextContainer>
+                <LinkContainer>
+                  <LightButton
+                    onClick={() => handleSelectEvent("Relationship")}
+                  >
+                    Relationship
+                  </LightButton>
+                  <LightButton onClick={() => handleSelectEvent("Exam")}>
+                    Exam
+                  </LightButton>
+                  <LightButton onClick={() => handleSelectEvent("Financial")}>
+                    Financial
+                  </LightButton>
+                </LinkContainer>
+              </>
+            ) : (
+              <>
+                <TextContainer>
+                  You're {emotion} about {event}
+                </TextContainer>
+                <Link to="/EmotionPlanet">
+                  <LightButton>Confirm Emotion</LightButton>
+                </Link>
+              </>
+            )}
+          </>
+        )}
       </InnerWrapper>
     </>
   );
