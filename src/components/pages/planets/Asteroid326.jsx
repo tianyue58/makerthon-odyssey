2178bca3a968cdd40d8f5804b9d4c02eccc89f25
Nature711 <e0665488@u.nus.edu>
2021-12-18 22:28:45 +0800

    Regroup files

---
+++ src/components/pages/planets/Asteroid326.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import image from "../../../images/326.png";
+import "../../../styles/Asteroid.css";
+
+function Asteroid326() {
+  return (
+    //   <InnerWrapper>
+    //       <div  style={sectionStyle} >
+    //              <h1>This is Asteroid 325!</h1>
+    //     </div>)
+    //   </InnerWrapper>
+    //     );
+    <>
+      <div class="container">
+        <img
+          src={image}
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        />
+        <div class="figcaption">
+          Welcome to Asteroid 326! 86 earthlings have visited this planet today.{" "}
+          <br />
+          <br />
+          Click on the buttons below to find out tips related to your issue.
+          Click on the "Let's Talk" sticker to see how to seek help from others.
+        </div>
+
+        <Link to="/A3261">
+          <button class="btn1">Button1</button>
+        </Link>
+
+        <Link to="/A3262">
+          <button class="btn2">Button2</button>
+        </Link>
+
+        <Link to="/A3263">
+          <button class="btn3">Button3</button>
+        </Link>
+
+        <Link to="/A3264">
+          <button class="btn4">Button4</button>
+        </Link>
+
+        <Link to="/A3265">
+          <button class="btn5">Button5</button>
+        </Link>
+
+        <Link to="/A3266">
+          <button class="btn6">Button6</button>
+        </Link>
+
+        <Link to="/A3267">
+          <button class="btn7">Talking Tips</button>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default Asteroid326;
